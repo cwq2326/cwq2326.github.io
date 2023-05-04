ef11714722c5d52ec09cb6c998a3702873dcdc28
cwq2326 <e0556260@u.nus.edu>
2023-05-04 14:12:48 +0800

    Add name to footer

--- src/components/Layouts/Footer.tsx
+++ src/components/Layouts/Footer.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Box, IconButton } from "@mui/material";
+import { Avatar, Box, IconButton, Typography } from "@mui/material";
 
 import { socials } from "../../data";
 
@@ -10,15 +10,26 @@ export default function Footer() {
         height: "10rem",
         bgcolor: "primary.main",
         display: "flex",
-        justifyContent: "center",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "space-evenly",
         gap: "1rem",
       }}
     >
+      <Box>
       {socials.map((el, idx: number) => (
         <IconButton target="__blank" href={el.link} key={idx}>
           <Avatar src={el.icon} variant="square" />
         </IconButton>
       ))}
+      </Box>
+      <Box>
+        <Typography variant="subtitle1"
+        sx={{
+          fontSize: ".5rem"
+        }}
+        >Designed and built by Chua Wen Quan</Typography>
+      </Box>
     </Box>
   );
 }
